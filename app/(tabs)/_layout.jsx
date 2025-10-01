9fb9c3a0995e216e05bb56949884e7822da4e2e1
Nandan Acharya <samarthachar18@gmail.com>
2025-10-01 12:09:28 +0530

    Update _layout.jsx

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -17,8 +17,7 @@ export default function TabLayout() {
       }}>
       <Tabs.Screen
         name="index"
-        options={{title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        options={{title: 'Home',tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
